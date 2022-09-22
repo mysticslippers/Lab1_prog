b6277b802318dce16836dafd24e2fe48cb89da91
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-09-22 11:10:09 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -5,6 +5,7 @@
  */
 
 public class Lab1 {
+
     public static void main(String[] args){
         //Объявление констант
         final float Fraction1 = (float) (1.0 / 3); //Дроби
@@ -33,21 +34,22 @@ public class Lab1 {
                 }
             }
         }
-
+        System.out.println("Выввод первого массива!");
         for(int i = 0; i < MinimumRange; i++){ //Вывод 1-ого массива
             System.out.print(" " + k[i] + " ");
         }
         System.out.println();
-
+        System.out.println();
+        System.out.println("Вывод второго массива!");
         for(int i = 0; i < MaximumRange; i++){ //Выовд 2-ого массива
             System.out.print(" " + x[i] + " ");
         }
         System.out.println();
-
-        for(int i = 0; i < MinimumRange; i++){ //Вывод конечного массива
+        System.out.println();
+        System.out.println("Вывод третьего массива!");
+        for(int i = 0; i < MinimumRange; i++){
             for(int j = 0; j < MaximumRange; j++){
-                String answer = String.format("%.2f", d[i][j]);
-                System.out.print(" " + answer + " ");
+                System.out.print(String.format("%-5.2f", (d[i][j])) + " ");
             }
             System.out.println();
         }
