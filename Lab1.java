45e0a1ce894a47a9a424f9b0dc8febb616a3014e
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-10-02 17:50:47 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -7,7 +7,7 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class Tasks {
+public class Lab1{
 
     static void FillingFirstArray(short[] arr, short MaximumValue) {
         short tmp = 21;
