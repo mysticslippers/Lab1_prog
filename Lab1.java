f598b57b83749ab2c367f09c11edfcf97a2fc605
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-10-02 17:06:31 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -7,61 +7,77 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class Lab1 {
+public class Tasks {
 
-    public static void main(String[] args){
-        //Объявление констант
-        final float Fraction1 = (float) (1.0 / 3); // Дроби
-        final float Fraction2 = (float) (2.0 / 3);
-        final short MaximumRange = 17;
-        final short MinimumRange = 11;
-
-        short[] k = {21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1};//Заполнение массива №1
-
-        float[] x = new float[MaximumRange]; //Заполнение массива для задания №2
-        for(int i = 0; i < MaximumRange; i++){
-            x[i] = (float) (Math.random() * (11 - (-15))) - 15;
+    static void FillingFirstArray(short[] arr, short MaximumValue) {
+        for (short i = 0; i < MaximumValue; i++) {
+            if ((MaximumValue - i) % 2 != 0) {
+                arr[i] = (short) (MaximumValue - i);
+            }
         }
+        for (short i = 0; i < (MaximumValue / 2); i++) {
+            short tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+    }
 
-        double[][] d = new double[MinimumRange][MaximumRange]; //Заполнение массива для задания №3
-        for(int i = 0; i < MinimumRange; i++){
-            for(int j = 0; j < MaximumRange; j++){
-                if(k[i] == 5){
-                    d[i][j] = Math.pow(((Math.pow((0.5 * (x[j] + Fraction1)), x[j]) / (Math.cbrt(x[j]) - Math.PI))) / 3, 3);
-                }
-                else if(k[i] == 1 || k[i] == 9 || k[i] == 11 || k[i] == 15 || k[i] == 21){
-                    d[i][j] = Math.asin(Math.pow(Math.E, Math.cbrt(Math.pow(-Math.sin(x[j]), 2))));
-                }
-                else{
-                    d[i][j] = Math.asin(Math.pow(Math.E, Math.cbrt(-Math.abs(Math.pow(Fraction2 * (3 + Math.cbrt(x[j])), Math.exp(Math.abs(x[j])))))));
+    static void FillingSecondArray(float[] arr, short MaximumRange) {
+        for (int i = 0; i < MaximumRange; i++) {
+            arr[i] = (float) (Math.random() * (11.0 - (-15.0))) - 15;
+        }
+    }
+
+    public static void main(String[] args) {
+        final float Fraction1 = (float) (1.0 / 3);
+        final float Fraction2 = (float) (2.0 / 3);
+        final short MinimumValue = 11;
+        final short MaximumValue = 17;
+
+        short[] k = new short[MinimumValue]; //Объявление и заполнение 1-ого массива.
+        Tasks.FillingFirstArray(k, MinimumValue);
+
+        float[] x = new float[MaximumValue]; //Объявление и заполнение 2-ого массива.
+        Tasks.FillingSecondArray(x, MaximumValue);
+
+        float[][] d = new float[MinimumValue][MaximumValue];
+        for (int i = 0; i < MinimumValue; i++) {
+            for (int j = 0; j < MaximumValue; j++) {
+                if (k[i] == 5) {
+                    d[i][j] = (float) Math.pow(((Math.pow((0.5 * (x[j] + Fraction1)), x[j]) / (Math.cbrt(x[j]) - Math.PI))) / 3, 3);
+                } else if (k[i] == 1 || k[i] == 9 || k[i] == 11 || k[i] == 15 || k[i] == 21) {
+                    d[i][j] = (float) Math.asin(Math.pow(Math.E, Math.cbrt(Math.pow(-Math.sin(x[j]), 2))));
+                } else {
+                    d[i][j] = (float) Math.asin(Math.pow(Math.E, Math.cbrt(-Math.abs(Math.pow(Fraction2 * (3 + Math.cbrt(x[j])), Math.exp(Math.abs(x[j])))))));
                 }
             }
         }
-
         System.out.println("Выввод первого массива!");
-        for(int i = 0; i < MinimumRange; i++){ //Вывод 1-ого массива
+        for (int i = 0; i < MinimumValue; i++) { //Вывод 1-ого массива
             System.out.print(" " + k[i] + " ");
         }
         System.out.println();
         System.out.println();
 
         System.out.println("Вывод второго массива!");
-        for(int i = 0; i < MaximumRange; i++){ //Выовд 2-ого массива
+        for (int i = 0; i < MaximumValue; i++) { //Выовд 2-ого массива
             System.out.print(" " + x[i] + " ");
         }
         System.out.println();
         System.out.println();
 
         System.out.println("Вывод третьего массива!");
-        for(int i = 0; i < MinimumRange; i++){  //Вывод 3-его массива
-            for(int j = 0; j < MaximumRange; j++){
+        for (int i = 0; i < MinimumValue; i++) {  //Вывод 3-его массива
+            for (int j = 0; j < MaximumValue; j++) {
                 String answer = String.valueOf(d[i][j]);
-                if(answer.equals("NaN") || d[i][j] < 0.0){ //На случай, если надо заменить NaN-ы
+                if(answer.equals("NaN")){
                     answer = "0.00";
                 }
                 else{
-                    BigDecimal result = BigDecimal.valueOf(d[i][j]);
-                    answer = String.valueOf(result.setScale(2, RoundingMode.DOWN));
+                    answer = String.valueOf(BigDecimal.valueOf(d[i][j]).setScale(2, RoundingMode.DOWN));
+                    if(answer.length() > 6){
+                        answer = "0.00";
+                    }
                 }
                 System.out.print(answer + " ");
             }
