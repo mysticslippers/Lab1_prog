c1dbd781e7fa9696a59b59ff104480946bcde4fe
mysticslippers <eternal.martyr13579@gmail.com>
2023-07-02 12:55:51 +0300

    Готовая лабораторная работа №1.

--- lab1/src/main/java/me/ifmo/Lab1.java
+++ lab1/src/main/java/me/ifmo/Lab1.java
@@ -1,5 +1,8 @@
 package me.ifmo;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Lab1{
 
     public static final int FIRST_ARRAY = 10;
@@ -39,5 +42,34 @@ public class Lab1{
         fillSecondArray(x);
         fillThirdArray(f, x, l);
 
+        System.out.println("Show the first array: ");
+        for(int i = 0; i < FIRST_ARRAY; i++){
+            System.out.print(" " + l[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Show the second array: ");
+        for(int i = 0; i < SECOND_ARRAY; i++){
+            System.out.print(" " + x[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Show the third array: ");
+        for(int i = 0; i < FIRST_ARRAY; i++){
+            for(int j = 0; j < SECOND_ARRAY; j++){
+                String answer = String.valueOf(f[i][j]);
+                if(answer.equals("NaN")){
+                    answer = "NaN" + " ";
+                }
+                else{
+                    answer = String.valueOf(BigDecimal.valueOf(f[i][j]).setScale(2, RoundingMode.DOWN));
+                    if(answer.length() > 5){
+                        answer = "0.00";
+                    }
+                }
+                System.out.print(answer + "\t");
+            }
+            System.out.println();
+        }
     }
 }
